b43da2b5054a05188e68d1e3763fdba40b314e5c
Jaimal Dullat <jaimaldullat@gmail.com>
2018-11-02 01:45:48 -0400

    Add some changes

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Searchbox from "../components/Searchbox";
+import CardList from "../components/CardList";
+import Scroll from "../components/Scroll";
+import "./App.css";
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			robots: [],
+			searchField: ""
+		};
+	}
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then(response => response.json())
+			.then(users => this.setState({ robots: users }));
+	}
+	onSearchChange = event => {
+		this.setState({ searchField: event.target.value });
+	};
+
+	render() {
+		const { robots, searchField } = this.state;
+		const filteredArray = robots.filter(robots => {
+			return robots.name
+				.toLowerCase()
+				.includes(searchField.toLowerCase());
+		});
+		return !robots.length ? (
+			<h1 className="f1"> Loading</h1>
+		) : (
+			<div className="tc">
+				<h1 className="f1"> RoboFriends</h1>
+
+				<Searchbox searchChange={this.onSearchChange} />
+				<Scroll>
+					<CardList robots={filteredArray} />
+				</Scroll>
+			</div>
+		);
+	}
+}
+
+export default App;
