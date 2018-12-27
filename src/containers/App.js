4a5a85c804509e243e5c7363c3c0d12305a96b0f
Jaimal Dullat <jaimaldullat@gmail.com>
2018-12-27 11:42:55 -0500

    Added All the Files

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Searchbox from "../components/Searchbox";
 import CardList from "../components/CardList";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
 class App extends Component {
@@ -15,10 +16,16 @@ class App extends Component {
 	componentDidMount() {
 		fetch("https://jsonplaceholder.typicode.com/users")
 			.then(response => response.json())
-			.then(users => this.setState({ robots: users }));
+			.then(users =>
+				this.setState({
+					robots: users
+				})
+			);
 	}
 	onSearchChange = event => {
-		this.setState({ searchField: event.target.value });
+		this.setState({
+			searchField: event.target.value
+		});
 	};
 
 	render() {
@@ -29,14 +36,15 @@ class App extends Component {
 				.includes(searchField.toLowerCase());
 		});
 		return !robots.length ? (
-			<h1 className="f1"> Loading</h1>
+			<h1 className="f1"> Loading </h1>
 		) : (
 			<div className="tc">
-				<h1 className="f1"> RoboFriends</h1>
-
+				<h1 className="f1"> RoboFriends </h1>
 				<Searchbox searchChange={this.onSearchChange} />
 				<Scroll>
-					<CardList robots={filteredArray} />
+					<ErrorBoundry>
+						<CardList robots={filteredArray} />
+					</ErrorBoundry>
 				</Scroll>
 			</div>
 		);
